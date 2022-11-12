6d19c8911a2cd3784074ac34198d07bbab1f1458
Hector Emiliano Llamas Salinas <117891364+Quick26@users.noreply.github.com>
2022-11-11 21:15:04 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        float Numero1 = 0;
+        float Numero2 = 0;
+        int respuesta = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite el primer numero");
+        Numero1 = sc.nextFloat();
+        System.out.println("Digite el segundo numero");
+        Numero2 = sc.nextFloat();
+        System.out.println("Digite el numero de la operacion a realizar.. 1-.Sumar 2-.Restar 3-.Multiplicar 4-.Dividir");
+        respuesta = sc.nextInt();
+
+        Operaciones oper = new Operaciones(Numero1, Numero2);
+
+        float resul = 0;
+
+        switch(respuesta){
+            case 1: resul = oper.suma(); break;
+            case 2: resul = oper.resta(); break;
+            case 3: resul = oper.multiplicacion(); break;
+            case 4: resul = oper.division(); break;
+
+
+        }
+
+        System.out.println("el resultado de la operacion es: "+ resul);
+    }
+}
